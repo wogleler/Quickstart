d18755009c404f901a354890e6cf18476ccb6db4
wogleler <victor.petru.2020@gmail.com>
2025-12-30 14:23:18 +0200

    i fuck ur mom

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Marcus2.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Marcus2.java
@@ -10,8 +10,10 @@ public class Marcus2 extends LinearOpMode{
     public void runOpMode(){
         Roti move = new Roti();
         Intake Itk = new Intake();
+        Outtake Out = new Outtake();
         move.Init(hardwareMap);
         Itk.Init(hardwareMap);
+        Out.Init(hardwareMap);
         while(!isStopRequested()&&opModeIsActive())
         {
             waitForStart();
@@ -19,6 +21,9 @@ public class Marcus2 extends LinearOpMode{
             {
                 move.Movement(gamepad1);
                 Itk.take(gamepad1);
+                Out.rotation(gamepad1, telemetry);
+                Out.DaHood(gamepad1, telemetry);
+                Out.fireinthehole(gamepad1, telemetry);
             }
         }
     }
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Outtake.java
@@ -0,0 +1,63 @@
+package org.firstinspires.ftc.teamcode;
+
+import android.service.chooser.ChooserTarget;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Gamepad;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
+public class Outtake {
+    DcMotor power;
+    Servo rotate, hood, help;
+    double hp=0.0;
+    public void Init(HardwareMap hardwareMap){
+        power = hardwareMap.get(DcMotor.class, "power");
+        rotate = hardwareMap.get(Servo.class, "rotate");
+        hood = hardwareMap.get(Servo.class, "hood");
+        help = hardwareMap.get(Servo.class, "help");
+        power.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        help.setDirection(Servo.Direction.REVERSE);
+        rotate.setPosition(0);
+        hood.setPosition(0);
+        help.setPosition(90);
+    }
+    public void rotation(Gamepad gamepad1, Telemetry telemetry){
+        if (gamepad1.right_trigger!=0){
+            rotate.setPosition(rotate.getPosition()+gamepad1.right_trigger);
+        } else if (gamepad1.left_trigger!=0) {
+            rotate.setPosition(rotate.getPosition()-gamepad1.left_trigger);
+        }
+        telemetry.addData("Rotate pos - ", rotate.getPosition());
+        telemetry.update();
+    }
+
+    public void DaHood(Gamepad gamepad1, Telemetry telemetry){
+        if (gamepad1.dpadDownWasPressed())
+            hp+=0.1;
+        if (gamepad1.dpadDownWasPressed())
+            hp-=0.1;
+        hood.setPosition(hp);
+        telemetry.addData("Hood pos - ", hood.getPosition());
+        telemetry.update();
+    }
+
+    public void fireinthehole(Gamepad gamepad1, Telemetry telemetry){
+        if (gamepad1.touchpadWasPressed()){
+            power.setPower(1);
+            help.setPosition(0);
+            telemetry.addData("Fire - ", 1);
+            telemetry.addData("Help - ", 0);
+            telemetry.update();
+        }
+        if (gamepad1.touchpadWasReleased()){
+            power.setPower(0);
+            help.setPosition(90);
+            telemetry.addData("Fire - ", 0);
+            telemetry.addData("Help - ", 90);
+            telemetry.update();
+        }
+    }
+}
